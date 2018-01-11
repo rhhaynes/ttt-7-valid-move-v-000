5f1ba9d3bd74b67c4667033c4a81f75bfe74f951
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-11 20:54:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,9 @@
 def valid_move?(array,index)
-  if position_taken?(array,index) && 
+  if position_taken?(array,index) && index.between?(0,8)
+    true
+  else
+    false
+  end
 end
 
 def position_taken?(array,index)
